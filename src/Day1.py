7375bcbdf6aab1b07300e3193279624087c76e42
KSMChaitanyaji <k.chaitanyaji@gmail.com>
2024-02-12 17:27:10 +0530

    day1

--- src/Day1.py
+++ src/Day1.py
@@ -12,7 +12,7 @@ actual_newuser_text = driver.find_element(By.XPATH,"//h2[text() = 'New User Sign
 if(actual_newuser_text == "New User Signup!"):
     print("pass")
 
-driver.find_element(By.XPATH,"//input[@type = 'text']").send_keys("chaitanya")
+driver.find_element(By.XPATH,"//input[@type = 'text']").send_keys("chaitanya") 
 driver.find_element(By.XPATH,"//input[@data-qa = 'signup-email']").send_keys("zxcv@gmail.com")
 driver.find_element(By.XPATH,"//button[@data-qa = 'signup-button']").click()
 signup_page_heading = driver.find_element(By.XPATH, "//b[contains(text(), 'Enter')]").text
@@ -20,4 +20,9 @@ signup_page_heading = driver.find_element(By.XPATH, "//b[contains(text(), 'Enter
 if(signup_page_heading == "ENTER ACCOUNT INFORMATION"):
     print("pass")
 
-driver.find_element
+
+driver.find_element(By.XPATH,"//input[@id = 'id_gender1']").click()
+driver.find_element(By.XPATH,"//input[@type = 'password']").send_keys("Abcd1234")
+
+days = driver.find_element(By.XPATH,"//select[@id = 'days']")
+days.send_keys("14")
