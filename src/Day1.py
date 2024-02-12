c563e28a62f9958355aaeddba7dfe717d8a25352
KSMChaitanyaji <k.chaitanyaji@gmail.com>
2024-02-12 16:38:37 +0530

    day1

---
+++ src/Day1.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://automationexercise.com/")
+login_signup = "//a[@href = '/login']"
+driver.find_element(By.XPATH,login_signup).click()
+time.sleep(5)
+actual_newuser_text = driver.find_element(By.XPATH,"//h2[text() = 'New User Signup!']").text
+
+if(actual_newuser_text == "New User Signup!"):
+    print("pass")
+
+driver.find_element(By.XPATH,"//input[@type = 'text']").send_keys("chaitanya")
+driver.find_element(By.XPATH,"//input[@data-qa = 'signup-email']").send_keys("zxcv@gmail.com")
+driver.find_element(By.XPATH,"//button[@data-qa = 'signup-button']").click()
+signup_page_heading = driver.find_element(By.XPATH, "//b[contains(text(), 'Enter')]").text
+
+if(signup_page_heading == "ENTER ACCOUNT INFORMATION"):
+    print("pass")
+
+driver.find_element
