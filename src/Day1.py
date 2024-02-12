629b338abbe5ceec4f9d6adc58da7453b690e0df
KSMChaitanyaji <k.chaitanyaji@gmail.com>
2024-02-12 23:09:42 +0530

    day1

--- src/Day1.py
+++ src/Day1.py
@@ -10,7 +10,7 @@ time.sleep(5)
 actual_newuser_text = driver.find_element(By.XPATH,"//h2[text() = 'New User Signup!']").text
 
 if(actual_newuser_text == "New User Signup!"):
-    print("pass")
+    print("pass",actual_newuser_text)
 
 driver.find_element(By.XPATH,"//input[@type = 'text']").send_keys("chaitanya") 
 driver.find_element(By.XPATH,"//input[@data-qa = 'signup-email']").send_keys("zxcv@gmail.com")
@@ -18,7 +18,7 @@ driver.find_element(By.XPATH,"//button[@data-qa = 'signup-button']").click()
 signup_page_heading = driver.find_element(By.XPATH, "//b[contains(text(), 'Enter')]").text
 
 if(signup_page_heading == "ENTER ACCOUNT INFORMATION"):
-    print("pass")
+    print("pass",signup_page_heading)
 
 
 driver.find_element(By.XPATH,"//input[@id = 'id_gender1']").click()
@@ -26,3 +26,19 @@ driver.find_element(By.XPATH,"//input[@type = 'password']").send_keys("Abcd1234"
 
 days = driver.find_element(By.XPATH,"//select[@id = 'days']")
 days.send_keys("14")
+month = driver.find_element(By.XPATH,"//select[@id = 'months']")
+month.send_keys("August")
+years = driver.find_element(By.XPATH,"//select[@id = 'years']")
+years.send_keys("2000")
+
+checkboxes = driver.find_elements(By.XPATH,"//input[@type = 'checkbox']")
+for i in checkboxes:
+    i.click()
+
+driver.find_element(By.XPATH,"//label[@for = 'first_name']") 
+driver.find_element(By.XPATH,"//input[@id = 'first_name']").send_keys("abc")
+driver.find_element(By.ID,"last_name").send_keys("zxc")
+driver.find_element(By.ID,"company").send_keys("nvbg")
+driver.find_element(By.ID,"address1").send_keys("dfghfk 345")
+
+
