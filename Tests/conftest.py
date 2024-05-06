211f81ddeb0420b6877c9315b32c580936cd1268
KSMChaitanyaji <k.chaitanyaji@gmail.com>
2024-05-06 23:07:51 +0530

    path changed

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -7,7 +7,7 @@ from config.config import TestData
 
 @pytest.fixture(scope = 'class')
 def init_driver(request):
-    web_driver = webdriver.Chrome()
+    web_driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver-mac/chromedriver')
     request.cls.driver = web_driver
     #web_driver.implicitly_wait(10)
     yield
