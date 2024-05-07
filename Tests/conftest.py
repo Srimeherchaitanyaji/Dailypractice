465bbc49d0ea7876a85748f6c15c7812cb8e99d0
KSMChaitanyaji <k.chaitanyaji@gmail.com>
2024-05-07 15:43:42 +0530

    some changes

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 import pytest
 
-from Pages.Registration_and_login_page import Registration_and_Login_Page
-from config.config import TestData
+
 
 
 @pytest.fixture(scope = 'class')
 def init_driver(request):
-    web_driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver-mac/chromedriver')
+    web_driver = webdriver.Chrome()
     request.cls.driver = web_driver
     #web_driver.implicitly_wait(10)
     yield
